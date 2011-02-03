f5e45d6d6fa5ed45eeb07d2f91a69c79fd318c44
proxorwebdev <proxor.webdev@gmail.com>
2011-02-04 02:34:36 +0300

    Django tutorial part 3. Use generic views

--- polls/views.py
+++ polls/views.py
@@ -6,29 +6,17 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
-
-def detail(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
-
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the poll voting form
-        return render_to_response('polls/detail.html', {
-            'poll': p,
+        return render_to_response('polls/poll_detail.html', {
+            'object': p,
             'error_message': "You didn't select a choice.",
         }, context_instance=RequestContext(request))
     else:
         selected_choice.votes +=1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id, )))
-
-def results(request, poll_id):
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/results.html', {'poll': p})
+        return HttpResponseRedirect(reverse('poll_results', args=(p.id, )))
