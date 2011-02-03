65383a93c4cb4d6cf816c4ca9524c3cd5dc7fa3d
proxorwebdev <proxor.webdev@gmail.com>
2011-02-04 02:04:03 +0300

    Django tutorial part 3. Write a simple form - vote for poll

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response, get_object_or_404
-from polls.models import Poll
-from django.http import Http404
+from polls.models import Choice, Poll
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.template import RequestContext
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -10,4 +12,23 @@ def index(request):
 
 def detail(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
-    return render_to_response('polls/detail.html', {'poll': p})
+    return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
+
+def vote(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id, )))
+
+def results(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/results.html', {'poll': p})
