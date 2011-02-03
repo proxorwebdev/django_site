5ea78ba7890c2a28d268424775cc48255d79f698
proxorwebdev <proxor.webdev@gmail.com>
2011-02-04 01:33:14 +0300

    Django tutorial part 3. Decoupling the URLconfs

--- urls.py
+++ urls.py
@@ -4,20 +4,7 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('polls.views',
-    # Example:
-    # (r'^django_site/', include('django_site.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    (r'^polls/$', 'index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'vote')
-)
-
-urlpatterns += patterns('',
-    # Uncomment the next line to enable the admin:
+urlpatterns = patterns('',
+    (r'^polls/', include('polls.urls')),
     (r'^admin/', include(admin.site.urls))
 )
