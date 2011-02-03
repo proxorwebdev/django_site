b4095962e0665cadc07d9a53b2b39f888b82bb7e
proxorwebdev <proxor.webdev@gmail.com>
2011-02-04 01:21:43 +0300

    Django tutorial part 3. Simplifying the URLconfs

--- urls.py
+++ urls.py
@@ -4,18 +4,20 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('polls.views',
     # Example:
     # (r'^django_site/', include('django_site.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    (r'^polls/$', 'polls.views.index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    (r'^polls/$', 'index'),
+    (r'^polls/(?P<poll_id>\d+)/$', 'detail'),
+    (r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
+    (r'^polls/(?P<poll_id>\d+)/vote/$', 'vote')
+)
 
+urlpatterns += patterns('',
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls))
 )
