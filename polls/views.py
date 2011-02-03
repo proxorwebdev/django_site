0d758ef898f61d8d7a113c76f343e9e18a11e019
proxorwebdev <proxor.webdev@gmail.com>
2011-02-04 00:05:54 +0300

    Django tutorial part 2. Rewriten html-list polls

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,8 @@
 # Create your views here.
 
-from django.template import Context, loader
+from django.shortcuts import render_to_response
 from polls.models import Poll
-from django.http import HttpResponse
-
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
