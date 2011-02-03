3185ae226f55e996f68cf33fe973f31f1e733661
proxorwebdev <proxor.webdev@gmail.com>
2011-02-03 23:01:55 +0300

    Django tutorial part 3. First view

--- polls/views.py
+++ polls/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from polls.models import Poll
+from django.http import HttpResponse
+
+
+def index(request):
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    output = '; '.join([p.question for p in latest_poll_list])
+    return HttpResponse(output)
--- urls.py
+++ urls.py
@@ -11,6 +11,11 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    (r'^polls/$', 'polls.views.index'),
+    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
