537512bea3f4eaee8c33b94c008e3ef006816740
proxorwebdev <proxor.webdev@gmail.com>
2011-02-04 00:27:33 +0300

    Django tutorial part 3. Show one poll

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,15 @@
 
 from django.shortcuts import render_to_response
 from polls.models import Poll
+from django.http import Http404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+def detail(request, poll_id):
+    try:
+        p = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404
+    return render_to_response('polls/detail.html', {'poll': p})
