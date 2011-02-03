3921252e850d454f90f4e396a73ae390e61bbb17
proxorwebdev <proxor.webdev@gmail.com>
2011-02-04 00:35:19 +0300

    Django tutorial part 3. Show one poll - rewriten

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
 from django.http import Http404
 
@@ -9,8 +9,5 @@ def index(request):
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    try:
-        p = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404
+    p = get_object_or_404(Poll, pk=poll_id)
     return render_to_response('polls/detail.html', {'poll': p})
