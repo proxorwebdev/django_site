c6a18ac6619f81f09276ac1edab5a885b64fc7ed
proxorwebdev <proxor.webdev@gmail.com>
2011-02-03 17:34:57 +0300

    Django tutorial part 2. Change form fields order

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,7 @@
 from polls.models import Poll
 from django.contrib import admin
 
-admin.site.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question']
+
+admin.site.register(Poll, PollAdmin)
