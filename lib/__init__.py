f3d9f5775b8ea8bbaff81ea722bb8c99f029d87d
proxorwebdev <proxor.webdev@gmail.com>
2011-02-06 22:59:49 +0300

    Small debug component

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,19 @@
 def inspect_model_vars(self):
     return "; ".join(["%s: %s" % (f.name, getattr(self,f.name)) for f in self._meta.fields])
+
+class Debug:
+    messages = []
+
+    @staticmethod
+    def add(message):
+        Debug.messages.append(message)
+
+class DebugMessagesMiddleware():
+    def process_response(self, request, response):
+        print 'Yahoo!!!'
+        messages = Debug.messages
+        debug_text = ''.join(messages)
+
+        response.content = debug_text + response.content
+
+        return response
